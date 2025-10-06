29552a7fef2a206863e78307ebaca8cc2b4ce8c7
Linford24 <linkladdy@gmail.com>
2025-10-06 21:24:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from router import innovation, innovator, auth
+from database.database import engine
+from models import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(auth.router)
+app.include_router(innovation.router)
+app.include_router(innovator.router)
