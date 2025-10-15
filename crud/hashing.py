298f5dba49943072f3c153727526f4e491279e8a
Linford24 <linkladdy@gmail.com>
2025-10-15 01:54:52 +0000

    Update hashing.py

--- crud/hashing.py
+++ crud/hashing.py
@@ -1,14 +1,8 @@
-import bcrypt
 from passlib.context import CryptContext
 
-# Add missing attribute that Passlib looks for
-if not hasattr(bcrypt, '__about__'):
-    class About:
-        __version__ = bcrypt.__version__
-    bcrypt.__about__ = About
-
-pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_ctx = CryptContext(schemes=["argon2"], deprecated="auto")
 
 class Hash():
-    def bcrypt(password: str):
+    def argon2(password: str):
+
         return pwd_ctx.hash(password)
